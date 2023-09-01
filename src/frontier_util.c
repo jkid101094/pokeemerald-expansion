5c2dfe1514b867c6fe5e7e87c540565e84813093
jkid101094 <jkid101094@gmail.com>
2023-09-01 18:03:12 -0500

    Revert "Merge branch 'saveblock' of https://github.com/ghoulslash/pokeemerald into Test-Branch"
    This reverts commit ffccf9e78a170d9c75dc4986a6130afab3ec0ca2, reversing
    changes made to bad083626ae861f6c8677891cf77cbd7825a644d.

--- src/frontier_util.c
+++ src/frontier_util.c
@@ -1643,13 +1643,9 @@ void CopyFrontierTrainerText(u8 whichText, u16 trainerId)
     switch (whichText)
     {
     case FRONTIER_BEFORE_TEXT:
-        #ifndef FREE_BATTLE_TOWER_E_READER
         if (trainerId == TRAINER_EREADER)
             FrontierSpeechToString(gSaveBlock2Ptr->frontier.ereaderTrainer.greeting);
         else if (trainerId == TRAINER_FRONTIER_BRAIN)
-        #else
-        if (trainerId == TRAINER_FRONTIER_BRAIN)
-        #endif
             CopyFrontierBrainText(FALSE);
         else if (trainerId < FRONTIER_TRAINERS_COUNT)
             FrontierSpeechToString(gFacilityTrainers[trainerId].speechBefore);
@@ -1659,15 +1655,11 @@ void CopyFrontierTrainerText(u8 whichText, u16 trainerId)
             BufferApprenticeChallengeText(trainerId - TRAINER_RECORD_MIXING_APPRENTICE);
         break;
     case FRONTIER_PLAYER_LOST_TEXT:
-        #ifndef FREE_BATTLE_TOWER_E_READER
         if (trainerId == TRAINER_EREADER)
         {
             FrontierSpeechToString(gSaveBlock2Ptr->frontier.ereaderTrainer.farewellPlayerLost);
         }
         else if (trainerId == TRAINER_FRONTIER_BRAIN)
-        #else
-        if (trainerId == TRAINER_FRONTIER_BRAIN)
-        #endif
         {
             CopyFrontierBrainText(FALSE);
         }
@@ -1693,9 +1685,7 @@ void CopyFrontierTrainerText(u8 whichText, u16 trainerId)
     case FRONTIER_PLAYER_WON_TEXT:
         if (trainerId == TRAINER_EREADER)
         {
-            #ifndef FREE_BATTLE_TOWER_E_READER
             FrontierSpeechToString(gSaveBlock2Ptr->frontier.ereaderTrainer.farewellPlayerWon);
-            #endif
         }
         else if (trainerId == TRAINER_FRONTIER_BRAIN)
         {
@@ -2225,7 +2215,6 @@ static void Print2PRecord(s32 position, s32 x, s32 y, struct RankingHall2P *hall
 
 static void Fill1PRecords(struct RankingHall1P *dst, s32 hallFacilityId, s32 lvlMode)
 {
-    #ifndef FREE_RECORD_MIXING_HALL_RECORDS
     s32 i, j;
     struct RankingHall1P record1P[HALL_RECORDS_COUNT + 1];
     struct PlayerHallRecords *playerHallRecords = AllocZeroed(sizeof(struct PlayerHallRecords));
@@ -2256,12 +2245,10 @@ static void Fill1PRecords(struct RankingHall1P *dst, s32 hallFacilityId, s32 lvl
     }
 
     Free(playerHallRecords);
-    #endif
 }
 
 static void Fill2PRecords(struct RankingHall2P *dst, s32 lvlMode)
 {
-    #ifndef FREE_RECORD_MIXING_HALL_RECORDS
     s32 i, j;
     struct RankingHall2P record2P[HALL_RECORDS_COUNT + 1];
     struct PlayerHallRecords *playerHallRecords = AllocZeroed(sizeof(struct PlayerHallRecords));
@@ -2292,7 +2279,6 @@ static void Fill2PRecords(struct RankingHall2P *dst, s32 lvlMode)
     }
 
     Free(playerHallRecords);
-    #endif
 }
 
 static void PrintHallRecords(s32 hallFacilityId, s32 lvlMode)
@@ -2342,7 +2328,6 @@ void ScrollRankingHallRecordsWindow(void)
 
 void ClearRankingHallRecords(void)
 {
-    #ifndef FREE_RECORD_MIXING_HALL_RECORDS
     s32 i, j, k;
 
     // UB: Passing 0 as a pointer instead of a pointer holding a value of 0.
@@ -2377,7 +2362,6 @@ void ClearRankingHallRecords(void)
             gSaveBlock2Ptr->hallRecords2P[j][k].winStreak = 0;
         }
     }
-    #endif
 }
 
 void SaveGameFrontier(void)
